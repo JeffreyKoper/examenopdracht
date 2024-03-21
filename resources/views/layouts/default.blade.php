66fc73b71618a75c2d732eca24e7d81f8c86aa4b
Jeffrey Koper <jkoper257@student.novacollege.nl>
2024-03-21 12:17:53 +0100

    added footer + styling, and a new color variable

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Clothing Store</title>
+    <title>Wardrobe Wonders</title>
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
@@ -21,10 +21,9 @@
                     <li><a href="#" class="nav-link">Contact</a></li>
                     @guest
                         <li><a href="{{route('login')}}" class="nav-link">Login</a></li>
-                        <li><a href="#" class="nav-link">Register</a></li>
+                        <li><a href="{{route('register')}}" class="nav-link">Register</a></li>
                     @endguest
                     @auth
-                    
                     <li><a href="{{route('logout')}}" class="nav-link">Log out</a></li>
                     @endauth    
                 </ul>
@@ -33,6 +32,19 @@
     </header>
 @yield('content')
 <footer>
-</
+    <div class="footer-content">
+        <h3>Stay connected with Wardrobe Wonders:</h3>
+        <ul>
+            <li>Follow us on social media for the latest trends and updates.</li>
+            <li>Contact us for any inquiries or assistance with your orders.</li>
+        </ul>
+    </div>
+    <div class="footer-thankyou">
+        <p>Thank you for choosing Wardrobe Wonders for your fashion needs!</p>
+    </div>
+    <div class="copyright">
+        <p>&copy; 2024 Wardrobe Wonders. All rights reserved.</p>
+    </div>
+</footer>
 </body>
 </html>
