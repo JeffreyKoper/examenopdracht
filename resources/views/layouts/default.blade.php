4e25bf2c4d6d279c3c4c90f30a4a1b1b5cbfc3ea
Jeffrey Koper <jkoper257@student.novacollege.nl>
2024-03-21 10:49:57 +0100

    added partials, and a attempt at the header

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,7 +8,25 @@
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
-
+    <header>
+        <div class="header">
+            <div class="logo">
+                <a href="/" class="logo-title"> Wardrobe Wonders</a>
+            </div>
+            <nav>
+                <ul class="navigation">
+                    <li><a href="#" class="nav-link">Home</a></li>
+                    <li><a href="#" class="nav-link">Shop</a></li>
+                    <li><a href="#" class="nav-link">About</a></li>
+                    <li><a href="#" class="nav-link">Contact</a></li>
+                    @guest
+                        <li><a href="#" class="nav-link">Login</a></li>
+                        <li><a href="#" class="nav-link">Register</a></li>
+                    @endguest
+                </ul>
+            </nav>
+        </div>
+    </header>
 @yield('content')
 <footer>
 </
