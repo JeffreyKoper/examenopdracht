141303c7672b4792c3a4818bddc396b3decca529
Jeffrey Koper <jkoper257@student.novacollege.nl>
2024-04-08 14:36:22 +0200

    formatted the code in ProductController, as well as modified the query to only select products where the stocks are above 0.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,18 +10,17 @@ class ProductController extends Controller
 {
   public function productspage()
   {
-    $products = Products::all();
+    $products = Products::where('stock', '>', 0)->get();
     return view('products', ['products' => $products]);
   }
 
   public function singleProduct($id)
-{
+  {
     $data = Products::find($id);
     $review_data = Reviews::join('users', 'reviews.user_id', '=', 'users.id')
-                           ->where('reviews.product_id', $id)
-                           ->select('reviews.*', 'users.name as user_name')
-                           ->get();
+      ->where('reviews.product_id', $id)
+      ->select('reviews.*', 'users.name as user_name')
+      ->get();
     return view('product', ['data' => $data, 'review_data' => $review_data]);
-}
-
+  }
 }
