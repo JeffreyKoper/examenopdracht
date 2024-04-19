502a978f19954601a4c0239a0ab2de59265aa02b
Jeffrey Koper <jkoper257@student.novacollege.nl>
2024-04-19 11:48:12 +0200

    restyled the homepage and added actual text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,10 @@
 @section('content')
     <div class="homepage">
         <div class="homepage-text">
-            <H1>HOMEPAGE</H1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti animi fugit blanditiis dolorem ex amet ad quo id? Dolorum mollitia quisquam ducimus nobis quam fugiat provident id autem possimus beatae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolores sit dolorem delectus numquam dignissimos ab libero consectetur, similique adipisci, ducimus earum molestiae Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum repellendus quasi nostrum illo aliquid incidunt dolor corrupti expedita soluta, quis quibusdam sed, facilis labore ratione sit alias commodi eligendi earum. quia ex nobis doloremque quis, corrupti pariatur. At placeat, obcaecati ullam impedit error voluptates provident aut veritatis laboriosam, ducimus atque libero numquam. Facilis est ut cumque commodi, dignissimos consectetur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe et deleniti nesciunt quidem quod voluptatem consectetur praesentium quisquam sit ipsa ipsum, explicabo, quo, soluta eum! Illum numquam quo laboriosam in.</p>
+            <h1>Welcome to Wardrobe Wonders</h1>
+            <p>Explore the world of fashion with Wardrobe Wonders. From chic outfits to casual essentials, we have everything you need to express your unique style.</p>
+            <p>Discover our latest collections and find pieces that inspire you. Whether you're looking for a statement piece or everyday basics, we've got you covered.</p>
+            <a href="{{@route('products')}}" class="btn btn-primary">Shop Now</a>
         </div>
         <div class="homepage-image">
             <img src="{{asset('img/winkel_producten.png')}}" alt="placeholder">
