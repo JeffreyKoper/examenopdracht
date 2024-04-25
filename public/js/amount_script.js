bd7eb079198155571ed441f4df5a5caee45492d3
Jeffrey Koper <jkoper257@student.novacollege.nl>
2024-04-25 12:00:20 +0200

    fixed a issue causing a user to be able to press the decrease or increase buttons whilest the ajax function was being run

--- public/js/amount_script.js
+++ public/js/amount_script.js
@@ -8,32 +8,47 @@ $(document).ready(function () {
 
     // Decrease button click event
     $(".decrease").on("click", function () {
-        var itemId = $(this).data("item-id");
+        var decreaseButton = $(this);
+        var increaseButton = decreaseButton.siblings(".increase");
+        var itemId = decreaseButton.data("item-id");
         var cartId = $(".cart-id").val(); // Get the cart_id from the hidden input
         console.log("Cart ID:", cartId); // Log the cart_id to the console for debugging
         var itemAmountElement = $("#itemAmount_" + itemId);
         var currentAmount = parseInt(itemAmountElement.text());
         if (currentAmount > 1) {
-            updateCartItem(itemId, cartId, currentAmount - 1);
+            disableButtons(decreaseButton, increaseButton);
+            updateCartItem(
+                itemId,
+                cartId,
+                currentAmount - 1,
+                enableButtons.bind(null, decreaseButton, increaseButton)
+            );
         }
     });
 
     // Increase button click event
     $(".increase").on("click", function () {
-        var itemId = $(this).data("item-id");
+        var increaseButton = $(this);
+        var decreaseButton = increaseButton.siblings(".decrease");
+        var itemId = increaseButton.data("item-id");
         var cartId = $(".cart-id").val(); // Get the cart_id from the hidden input
         console.log("Cart ID:", cartId); // Log the cart_id to the console for debugging
         var itemAmountElement = $("#itemAmount_" + itemId);
         var currentAmount = parseInt(itemAmountElement.text());
         var stock = parseInt($("#ItemAmountStock_" + itemId).val());
         if (currentAmount < stock) {
-            updateCartItem(itemId, cartId, currentAmount + 1);
+            disableButtons(increaseButton, decreaseButton);
+            updateCartItem(
+                itemId,
+                cartId,
+                currentAmount + 1,
+                enableButtons.bind(null, increaseButton, decreaseButton)
+            );
         }
     });
 
     // Function to update cart item via AJAX
-    // Function to update cart item via AJAX
-    function updateCartItem(itemId, cartId, newAmount) {
+    function updateCartItem(itemId, cartId, newAmount, callback) {
         $.ajax({
             url: "/update-cart-item",
             method: "POST",
@@ -57,11 +72,28 @@ $(document).ready(function () {
                 // Handle AJAX errors
                 console.error(xhr.responseText);
             },
+            complete: function () {
+                if (typeof callback === "function") {
+                    callback();
+                }
+            },
         });
     }
 
     // Function to update the count in the navbar
     function updateNavbarProductCount(count) {
         $(".cart-count").text(count);
+    }
+
+    // Function to disable buttons
+    function disableButtons(button1, button2) {
+        button1.prop("disabled", true);
+        button2.prop("disabled", true);
+    }
+
+    // Function to enable buttons
+    function enableButtons(button1, button2) {
+        button1.prop("disabled", false);
+        button2.prop("disabled", false);
     }
 });
